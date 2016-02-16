9a0c08c15482e6a05b86336bd36df8a963b3f786
WilliamZ <william.zhou@hotmail.com>
2016-02-16 18:15:51 +1300

    Add highlight and onpick callback on cpr

--- src/cpr/cpr.js
+++ src/cpr/cpr.js
@@ -2,12 +2,17 @@ module.exports = function(opts) {
   opts = opts || {};
   var columns = opts.columns || 4;
   var palette = opts.palette || [];
+  var onpick = opts.onpick || function() {};
+  var blockWidth = 20;
+  var blockHeight = 20;
 
   var container = document.createElement('div');
   container.style.position = 'absolute';
   container.style.left = '20px';
   container.style.bottom = '20px';
   document.body.appendChild(container);
+
+  var blocks = [];
 
   for (var i = 0; i < palette.length; i++) {
     addColorBlock(i, palette[i]);
@@ -17,18 +22,19 @@ module.exports = function(opts) {
   function addColorBlock(index, color) {
     var div = document.createElement('div');
     div.style.position = 'absolute';
-    div.style.left = getColumn(index) * 20 + 'px';
-    div.style.top = getRow(index) * 20 + 'px';
-    div.style.width = '20px';
-    div.style.height = '20px';
+    div.style.left = getColumn(index) * blockWidth + 'px';
+    div.style.top = getRow(index) * blockHeight + 'px';
+    div.style.width = blockWidth + 'px';
+    div.style.height = blockHeight + 'px';
     div.style.backgroundColor = color;
     div.style.display = 'inline-block';
     container.appendChild(div);
+    blocks[index] = div;
   };
 
   function updateContainer() {
-    container.style.width = columns * 20 + 'px';
-    container.style.height = Math.ceil(palette.length / columns) * 20 + 'px';
+    container.style.width = columns * blockWidth + 'px';
+    container.style.height = Math.ceil(palette.length / columns) * blockHeight + 'px';
   };
 
   function getRow(index) {
@@ -38,6 +44,39 @@ module.exports = function(opts) {
   function getColumn(index) {
     return index % columns;
   };
+
+  function getIndex(row, column) {
+    return row * columns + column;
+  };
+
+  var highlightDiv = null;
+
+  function highlight(index) {
+    if (highlightDiv == null) {
+      highlightDiv = document.createElement('div');
+      highlightDiv.style.position = 'absolute';
+      highlightDiv.style.width = blockWidth + 'px';
+      highlightDiv.style.height = blockHeight + 'px';
+      highlightDiv.style.display = 'inline-block';
+      highlightDiv.style.border = '1px solid #FFFFFF';
+      container.appendChild(highlightDiv);
+    }
+
+    highlightDiv.style.left = getColumn(index) * blockWidth - 1 + 'px';
+    highlightDiv.style.top = getRow(index) * blockHeight - 1 + 'px';
+  };
+
+  container.addEventListener('mousedown', function(e) {
+    var mouseX = e.pageX - container.offsetLeft;
+    var mouseY = e.pageY - container.offsetTop;
+    var row = Math.floor(mouseY / blockHeight);
+    var column = Math.floor(mouseX / blockWidth);
+    var index = getIndex(row, column);
+    var color = palette[index];
+    highlight(index);
+
+    onpick(color);
+  });
 
   return container;
 };

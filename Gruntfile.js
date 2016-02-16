dab177c303befb40514a385f2a150e30e41187fe
WilliamZ <william.zhou@hotmail.com>
2016-02-16 17:26:27 +1300

    Add grunt dev task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,24 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    shell: {
+      build: {
+        command: 'npm run build'
+      }
+    },
+    serve: {
+
+    },
+    concurrent: {
+      dev: ['serve', 'shell'],
+      options: {
+        logConcurrentOutput: true
+      }
+    }
+  });
+
   grunt.loadNpmTasks('grunt-serve');
-  grunt.registerTask('default', ['serve']);
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.registerTask('default', ['concurrent:dev']);
 };
